423e56523b3f4835567ee20434b54a411f25467b
timofmax <timofmax@gmail.com>
2023-03-09 04:33:03 +0300

    ready

--- day3/day3/ViewController.swift
+++ day3/day3/ViewController.swift
@@ -8,13 +8,11 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    
     private let slider = UISlider(frame: CGRect(x: 20, y: 20,
                                                 width: 40, height: 40))
     private let rectangle: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-    private var degree = CGFloat(Double.pi/180)
-    
-    var sliderMaxValue = Float(UIScreen.main.bounds.width)
+    private var originViewSize: CGRect = .zero
+    private var sliderMaxValue = Float(UIScreen.main.bounds.width)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,20 +20,24 @@ final class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        setupSlider()
-        setupRectangle()
-        animateView()
+        setupViews()
     }
     
-    private func setupSlider() {
+    private func setupViews() {
         view.addSubview(slider)
+        view.addSubview(rectangle)
         let marginsGuide = view.layoutMarginsGuide
         slider.translatesAutoresizingMaskIntoConstraints = false
+        rectangle.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             slider.leadingAnchor.constraint(equalTo: marginsGuide.leadingAnchor),
             slider.trailingAnchor.constraint(equalTo: marginsGuide.trailingAnchor),
             slider.centerXAnchor.constraint(equalTo: marginsGuide.centerXAnchor),
-            slider.centerYAnchor.constraint(equalTo: marginsGuide.centerYAnchor, constant: -UIScreen.main.bounds.height/4.5)
+            slider.centerYAnchor.constraint(equalTo: marginsGuide.centerYAnchor, constant: -UIScreen.main.bounds.height/7),
+            rectangle.leadingAnchor.constraint(equalTo: marginsGuide.leadingAnchor),
+            rectangle.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -UIScreen.main.bounds.height/3),
+            rectangle.widthAnchor.constraint(equalToConstant: 80),
+            rectangle.heightAnchor.constraint(equalToConstant: 80)
         ])
         slider.minimumValue = 0
         slider.value = 0
@@ -43,52 +45,35 @@ final class ViewController: UIViewController {
         slider.tintColor = .lightGray
         slider.thumbTintColor = .white
         slider.addTarget(self, action: #selector(animateView), for: .valueChanged)
-        slider.addTarget(self, action: #selector(animateSlider), for: .touchUpOutside)
-    }
-    
-    private func setupRectangle() {
-        view.addSubview(rectangle)
-        let marginsGuide = view.layoutMarginsGuide
-        rectangle.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            rectangle.leadingAnchor.constraint(equalTo: marginsGuide.leadingAnchor),
-            rectangle.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -UIScreen.main.bounds.height/3),
-            rectangle.widthAnchor.constraint(equalToConstant: 80),
-            rectangle.heightAnchor.constraint(equalToConstant: 80)
-        ])
+        slider.addTarget(self, action: #selector(animateSlider), for: .touchUpInside)
         rectangle.backgroundColor = .systemBlue
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        let viewMargins = view.layoutMargins
-        slider.minimumValue = Float(viewMargins.left) + Float(rectangle.bounds.width/2)
-        slider.maximumValue = sliderMaxValue - Float(viewMargins.right) - Float(rectangle.bounds.width/2)
+        rectangle.layer.masksToBounds = true
+        rectangle.layer.cornerRadius = 10
     }
     
     @objc
-    private func animateView(withDuration duration: TimeInterval = 0.7) {
-
-        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 1,
-                       initialSpringVelocity: 1, options: .curveEaseInOut) {
-            let current = self.rectangle.center
-            self.rectangle.center = CGPoint(x: CGFloat(self.slider.value), y: current.y)
-        }
+    private func animateView() {
+        UIView.animate(withDuration: 0.3, delay: 0, options: [.curveLinear], animations: { [self] in
+            let first = CGAffineTransform(rotationAngle: (.pi / 2) * CGFloat(slider.value))
+            let second = CGAffineTransform(scaleX: 1 + CGFloat(slider.value / 2), y: 1 + CGFloat(slider.value / 2))
+            let third = CGAffineTransform(translationX: originViewSize.minX + (CGFloat(slider.value) * ((view.frame.width - 8) - rectangle.frame.width)), y: originViewSize.minY + CGFloat(CGFloat(slider.value) * ((rectangle.frame.width - originViewSize.width) / 2)))
+            rectangle.transform = CGAffineTransformConcat(CGAffineTransformConcat(second, first), third )
+        })
     }
     
     @objc
     private func animateSlider() {
-        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 1,
-                       initialSpringVelocity: 0, options: .curveEaseInOut) {
-            let current = self.rectangle.center
-            self.slider.setValue(self.sliderMaxValue, animated: true)
-            self.rectangle.center = CGPoint(x: CGFloat(self.slider.value), y: current.y)
+        Timer.scheduledTimer(withTimeInterval: 0.005, repeats: true) { [self] timer in
+            let range =  slider.maximumValue - slider.minimumValue
+            let step = range/200
+            let nextStep = slider.value + step
+            if step <= slider.maximumValue && slider.value != slider.maximumValue {
+                slider.setValue(nextStep, animated: true)
+                animateView()
+            } else {
+                timer.invalidate()
+            }
         }
-    }
-    
-    func applyMultiple() {
-        rectangle.transform = CGAffineTransform(rotationAngle: .pi / 2)
-            .concatenating(CGAffineTransform(scaleX: 1.5, y: 1.5))
     }
 }
 
